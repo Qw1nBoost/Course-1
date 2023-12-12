01a43df71888f33c662db002a2f64586fc67c602
Andrey <97914436+Qw1nBoost@users.noreply.github.com>
2023-12-12 12:23:18 +0200

    Add files via upload

---
+++ Cycles/Cycles2.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    if (num > 1000000000) {
+        return 0;
+    }
+
+    int maxCount = 0; // максимальное кол-во встречающихся цифр
+    int mostCommonDigit = 0; // самая часто встречающаяся цифра
+
+    for (int i = 0; i < 10; i++) {
+        int count = 0; // счетчик для текущей цифры
+        int tempNumber = num; // временная переменная
+
+        while (tempNumber != 0) {
+            int digit = tempNumber % 10;
+            if (digit == i) {
+                count++;
+            }
+            tempNumber /= 10;
+        }
+
+        if (count > maxCount) {
+            maxCount = count;
+            mostCommonDigit = i;
+        }
+    }
+
+    std::cout << "The most common number: " << mostCommonDigit << '\n';
+
+    return 0;
+}
+
